469456ef377413e1fffe87135d3d908f9f54c794
Luan Oliveira <luanoliveira17@hotmail.com>
2020-09-25 11:36:19 -0300

    Modularizando o codigo

---
+++ models/produtos.go
@@ -0,0 +1,43 @@
+package models
+
+import "../db"
+
+// Produto padrão da loja
+type Produto struct {
+	Nome, Descricao string
+	Preco           float64
+	ID, Quantidade  int
+}
+
+// BuscaTodosOSProdutos é responsavel por fazer um select all em produtos
+func BuscaTodosOsProdutos() []Produto {
+	db := db.ConectaComBancoDeDados()
+
+	selectDeTodosOsProdutos, err := db.Query("Select * from produtos")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	p := Produto{}
+	produtos := []Produto{}
+
+	for selectDeTodosOsProdutos.Next() {
+		var id, quantidade int
+		var nome, descricao string
+		var preco float64
+
+		err = selectDeTodosOsProdutos.Scan(&id, &nome, &descricao, &preco, &quantidade)
+		if err != nil {
+			panic(err.Error())
+		}
+
+		p.Nome = nome
+		p.Descricao = descricao
+		p.Preco = preco
+		p.Quantidade = quantidade
+
+		produtos = append(produtos, p)
+	}
+	defer db.Close()
+	return produtos
+}
